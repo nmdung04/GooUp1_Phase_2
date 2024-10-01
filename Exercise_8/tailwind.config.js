1b17eb3246dac0a3c32f0cf9042f30ca55de7071
nmdung04 <nmdung04@gmail.com>
2024-10-02 02:13:51 +0700

    update ex8 complete

--- Exercise_8/tailwind.config.js
+++ Exercise_8/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html",       // Để Tailwind quét file index.html ở gốc
+    "./css/**/*.{html,js}"],
   theme: {
     extend: {},
   },
