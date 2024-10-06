6bbd6e75257bee67088c5020ab25cb29abb6c5be
nmdung04 <nmdung04@gmail.com>
2024-10-06 08:49:10 +0700

    modified ex10 -ver1

---
+++ Exercise_10/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",       // Để Tailwind quét file index.html ở gốc
+    "./css/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
